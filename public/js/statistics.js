c082e5966f63011b452e0e4288b2c26ae078cd02
Evgeny Anoykin <hamway0611@gmail.com>
2013-04-27 23:18:01 +0600

    Изменен шаблон текта #NODE-3 in progrees

--- public/js/statistics.js
+++ public/js/statistics.js
@@ -5,3 +5,22 @@
  * Time: 18:46
  * To change this template use File | Settings | File Templates.
  */
+function getDateList(){
+
+}
+
+function renderDataList() {
+    var list = getDateList('now');
+}
+
+$(document).ready(function(){
+    $('.alert .close').click(function(){
+       $(this).parent().animate({opacity:0, height:0});
+    });
+
+    $('.nav>li>a.disable').click(function(){
+        return false;
+    });
+
+    renderDateList();
+});
